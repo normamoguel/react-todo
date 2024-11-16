201cae31ab704c48c612e398b4b6f1d0892217a0
norma <norma.moguel@gmail.com>
2024-11-16 14:30:31 -0500

    changes about lesson 1_3

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,10 +1,18 @@
 
-function AddTodoForm ()
-{
+function AddTodoForm (props)
+{  
+    function handleAddTodo(event){
+        event.preventDefault();
+        let todoTitle=event.target.title.value;
+        console.log(todoTitle);
+        props.onAddTodo(todoTitle);
+        event.target.reset();
+   }
    return(
-    <form >
+    
+    <form onSubmit={handleAddTodo} >
         <label htmlFor="todoTitle" >Title</label>
-        <input id="todoTitle"></input>
+        <input name="title" id="title"></input>
         <button type="submit" > Add </button>
     </form>
 
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,4 @@
-
+import TodoListItem from "./TodoListItem";
 
 const todoList= [{id:1,title:"Complete assignment 1"},{id:2,title:"Complete assignment 2"},{id:3,title:"Complete assignment 3"}];
 
@@ -7,7 +7,8 @@ function TodoList()
     <ul>
           {
             todoList.map(item=>{
-              return <li key={item.id} >{item.title}</li>
+              return (   <TodoListItem key={item.id} todo={item} />);
+              
             })
           }
       </ul>
