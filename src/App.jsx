e3fb12d4795bb38298382265987be5396e5f8e1a
norma <norma.moguel@gmail.com>
2025-01-12 19:30:01 -0500

    homework lesson_1_9

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Fragment, useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
@@ -88,9 +88,27 @@ function App() {
 
   return (
     <Fragment>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo}/>
-      {isLoading?(<p>Loading...</p>): <TodoList todoList={todoList}  onRemoveTodo={removeTodo}/>}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" 
+                 element={ 
+                        <>
+                          <h1>Todo List</h1>
+                          <AddTodoForm onAddTodo={addTodo}/>
+                           {isLoading?(<p>Loading...</p>): <TodoList todoList={todoList}  onRemoveTodo={removeTodo}/>}
+                        </>
+                  }
+          />
+          <Route path="/new"
+                 element={
+                  <>
+                          <h1>New Todo List</h1>
+                  </>      
+                 }
+          
+          />
+        </Routes>
+      </BrowserRouter>
       
     </Fragment>
   )
